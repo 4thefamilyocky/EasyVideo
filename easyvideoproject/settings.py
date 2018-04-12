04d538eee6d0e56a7ad98e188e72cdd792029024
tazbingor <852353298@qq.com>
2018-04-12 22:01:19 +0800

    创建项目并配置文件

--- easyvideoproject/settings.py
+++ easyvideoproject/settings.py
@@ -97,9 +97,9 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
-
+# 语言
 LANGUAGE_CODE = 'zh-hans'
-
+# 时区
 TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
