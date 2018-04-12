bc211325ce2251fb32375a10e9e8c17d9e8f961a
tazbingor <852353298@qq.com>
2018-04-12 22:36:43 +0800

    首页view

--- video/views.py
+++ video/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+
 
 # Create your views here.
+def index(request):
+    '''
+    首页视图
+    :param request:
+    :return:
+    '''
+    return HttpResponse('这是首页')
