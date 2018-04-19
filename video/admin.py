84d3c3e8918f6a336b034f1f81ce537ce6dcab13
tazbingor <852353298@qq.com>
2018-04-19 18:54:11 +0800

    添加模型数据

--- video/admin.py
+++ video/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
+from .models import *
+
 
 # Register your models here.
+class CateAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+
+
+class VideoAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'link', 'img', 'passwd', 'cate', 'views', 'create_time', 'status']
+    list_editable = ['status', 'passwd']
+    list_filter = ['cate', 'status']
+    search_fields = ['introduce']
+
+
+class LabelAdmin(admin.ModelAdmin):
+    list_display = ['id', 'video', 'lable']
+
+
+class SetAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'video']
+    list_filter = ['name']
+
+
+class HistoryAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'video', 'view_date']
+    list_filter = ['user', 'video', 'view_date']
+
+
+admin.site.register(Cate, )
+admin.site.register(Video, )
+admin.site.register(Label, )
+admin.site.register(Set, )
+admin.site.register(History, )
